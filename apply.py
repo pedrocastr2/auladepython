74fd9627b151f9fe7fe132d2749dfe26921b188e
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2024-08-30 21:08:51 -0300

    Add files via upload

---
+++ apply.py
@@ -0,0 +1,12 @@
+
+
+print("Cafe express \n")
+print('------------------------')
+print('Olá,Bem vindo, Usuário.')
+print('1. Cadastrar Usuário')
+print('2. Ver Reservas')
+print('3. Ver o menu')
+print('4. sair')
+print('------------------------\n')
+opcao_escolhida =input('Digite a opção escolhida:  ')
+print('Você escolheu a opção : ', opcao_escolhida)
